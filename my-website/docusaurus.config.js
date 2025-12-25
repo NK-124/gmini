f5722215ce418909de88b923bbfa519f18e1e0ac
Goku <goku@kamehouse.com>
2025-12-25 21:02:15 +0500

    fix: Update baseUrl in docusaurus.config.js to '/'

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/robotics-book/',
+  baseUrl: '/',
 
 
 
